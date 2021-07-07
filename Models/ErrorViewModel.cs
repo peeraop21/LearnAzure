59d7daf4867c422b15e8f45339e10f666787da7e
DESKTOP-3U1IEFS\ROIN <peeran@rvpnet.onmicrosoft.com>
2021-07-07 08:47:22 +0700

    Created asp.net core MVC 5.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
